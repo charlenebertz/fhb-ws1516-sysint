16677b7e02f74821aa63f45c7b35564ef3e7fb33
Charlene Bertz <charlenebertz@arcor.de>
2015-12-12 12:50:49 +0100

    updated pybuilder configuration

--- build.py
+++ build.py
@@ -1,9 +1,14 @@
-from pybuilder.core import use_plugin
+from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
+use_plugin("python.install_dependencies")
 use_plugin("python.unittest")
 #use_plugin("python.integrationtest")
-use_plugin("python.distutils")
-use_plugin("python.install_dependencies")
+#use_plugin("python.distutils")
 
 default_task = "publish"
+
+@init
+def initialize(project):
+    project.build_depends_on('boto3')
+    project.build_depends_on('bottle')
