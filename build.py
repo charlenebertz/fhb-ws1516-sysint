9587881d70f812ec05de4b64746e15ebfe54c253
Charlene Bertz <charlenebertz@arcor.de>
2015-12-13 22:16:00 +0100

    updated build configuration
    added integrationtest + turned it off due to not working on windows 10

--- build.py
+++ build.py
@@ -3,7 +3,8 @@ from pybuilder.core import use_plugin, init
 use_plugin("python.core")
 use_plugin("python.install_dependencies")
 use_plugin("python.unittest")
-#use_plugin("python.distutils")
+#use_plugin("python.integrationtest")
+use_plugin("python.distutils")
 
 default_task = "publish"
 
