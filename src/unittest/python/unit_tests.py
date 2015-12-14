1d91511a2028e4d5ca4fbe4c41b39b6a2c82b519
Charlene Bertz <charlenebertz@arcor.de>
2015-12-14 10:50:10 +0100

    travis config #1

--- src/unittest/python/unit_tests.py
+++ src/unittest/python/unit_tests.py
@@ -6,7 +6,7 @@ import boto3
 try:
     import main as mainfile #path used by pybuilder
 except:
-    import src.main.python.main as mainfile #path used by travis
+    import main.python.main as mainfile #path used by travis
 
 class Unittest(TestCase):
     def test_erstelleAnwendungsHTML(self):
