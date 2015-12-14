0714cf3d1d1e08b349dda39b9e88943c0b5e8eaf
Charlene Bertz <charlenebertz@arcor.de>
2015-12-14 10:35:01 +0100

    updated pybuilder configuration

--- build.py
+++ build.py
@@ -3,7 +3,6 @@ from pybuilder.core import use_plugin, init
 use_plugin("python.core")
 use_plugin("python.install_dependencies")
 use_plugin("python.unittest")
-#use_plugin("python.integrationtest")
 use_plugin("python.distutils")
 
 default_task = "publish"
