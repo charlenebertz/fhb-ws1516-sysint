2ee61a3e6f530a3584a0602a5b9c937f25672788
Charlene Bertz <charlenebertz@arcor.de>
2015-12-14 10:21:58 +0100

    Revert "updated test file"
    This reverts commit aaa3fdadfc4def483ad9b004f7be6d41ee59360c.

--- src/integrationtest/python/integration_tests.py
+++ src/integrationtest/python/integration_tests.py
@@ -5,7 +5,7 @@ from time import sleep
 try:
     from main import run_server
 except:
-    from main.python.main import run_server
+    from src.main.python.main import run_server
 import threading
 import requests
 
